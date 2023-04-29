8cc0c19fac0e8004f1b325e98ec547f7ace4bee2
MBisciotti <matthewbisc@yahoo.com>
2023-04-29 17:09:19 -0400

    part 2 done lab 6

--- Black-Steel-Langley.Api/Program.cs
+++ Black-Steel-Langley.Api/Program.cs
@@ -15,6 +15,16 @@ builder.Services.AddDbContext<StoreContext>(options =>
     options.UseSqlite("Data Source=../Registrar.sqlite", 
     b => b.MigrationsAssembly("Black.Steel.Langley.Api"))
 );
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolict(builder =>
+    {
+        //If error with localhost, try changning this vvv 
+        builder.WithOrigins("http://localhost:3000")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -28,6 +38,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors();
 
 app.UseAuthorization();
 
